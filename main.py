f6d40c477885f5b0d7b237f370f97c8b625b80ce
Ervin-Capuno <ervinreycapuno23@gmail.com>
2023-08-04 07:20:19 -0400

    added code for inserting data to database

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from dbFile.Database import Database
 from utils.ErrorClass import ErrorClass
 
 def pick():
-    return int(input("What do you want do do: "))
-    
+    ErrorClass.data_type_error('your choice: ', int)
 
 def menu():
     print("""
@@ -12,13 +11,44 @@ def menu():
         2. Read Data
         3. Delete Data
         4. Search Data
+        5. Exit
     """)
 
 
 def main():
-    name = input("What is your name: ")
-    sanitized_name = Database.sanitize_input(name)
-    print(sanitized_name)
+    db = Database()
+
+    run = True
+
+   while run:
+        choose = pick()
+        if choose == 1:
+            firstName = input("What is your first name: ")
+            lastName = input("What is your last name: ")
+            password = input("What is your password")
+            age = int(input("What is your age: "))
+            address = input("What is your age: ")
+            school = input("What school do you study: ")
+
+            
+            db.insert_data("persons", {'password': password, 'firstName' : firstName, 'lastName' : lastName,
+                'age' : age, 'address' : address, 'school' : school})
+
+            cont = input("Do you want to fill up your vital signs: ")
+
+
+        elif choose == 2:
+
+        elif choose == 3:
+
+        elif choose == 4:
+
+        elif choose == 5:
+            run = False
+        
+        else:
+            print("Invalid choice!, Please Try Again!")
+
 
 if __name__ == '__main__':
     main()
